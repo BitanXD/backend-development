08e146c8be4f7aa49dc590e47be2c0e8ae941cf2
BitanXD <bitansarkar088@gmail.com>
2024-08-14 04:23:31 +0530

    controller configuration complete

--- src/app.js
+++ src/app.js
@@ -1,20 +1,32 @@
-import express from 'express';
-import cors from 'cors'
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 //configurations
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({
-    extended: true
-}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+app.use(cookieParser());
 
-export {app}
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter); //we will not use app.get as we have set up a middleware to handle it
+// http://localhost:8000/api/v1/users/register
+
+export { app };
